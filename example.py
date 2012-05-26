ec605f3b12c7749940f62d4a2f0ba35ce2e13c34
Richard Marko <rissko@gmail.com>
2012-05-26 13:55:39 +0200

    Better example

--- example.py
+++ example.py
@@ -5,14 +5,15 @@ import mlp
 a = mlp.MLP()
 a.add_layer(mlp.Layer(2))
 a.add_layer(mlp.Layer(2))
-a.add_layer(mlp.Layer(1))
+a.add_layer(mlp.Layer(2))
 
 a.init_network()
 a.layers[0].values = [1,1]
 a.layers[0].weights[0]=[0.5,0.2]
 a.layers[0].weights[1]=[0.1,-0.7]
-a.layers[1].weights[0]=[0.1]
-a.layers[1].weights[1]=[-0.1]
+a.layers[1].weights[0]=[0.1, 0.2]
+a.layers[1].weights[1]=[-0.1, 0.2]
 a._activate()
+a._back_propagate([1, 0])
 
 print a
